c319ae5320160075e990b6ee136efa200146897f
frehiywot nega <fr.nega@gmail.com>
2022-11-22 12:24:15 +0300

    added home page and like buttons

---
+++ src/modules/displayfooditem.js
@@ -0,0 +1,34 @@
+// import displayPopup from './popupWindow.js';
+import { getnumberofLikes, Interactlikebutton } from './likeitems.js';
+import itemsCounter from './itemCounter.js';
+
+const recipeCount = document.querySelector('.item-count');
+const recipeCards = document.querySelector('.recipe-cards');
+
+const displayfoodCards = async () => {
+  fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+    .then((response) => response.json())
+    .then((data) => {
+      let html = '';
+      data.meals.forEach((meal) => {
+        html += `<div class="card" id="${meal.idMeal}">
+               <div class="card-img">
+                   <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+               </div>
+               <div class="card-info">
+                   <h2>${meal.strMeal}</h2>
+                   <ul>
+                       <li><button id=${meal.idMeal}  class="comment-btn" type="button">Comments</button></li>
+                       <li><button class="like-btn"><i class="fa-regular fa-heart"></i><span class="like-count">0</span></button></li>
+                   </ul>
+               </div>
+             </div>`;
+        recipeCards.innerHTML = html;
+      });
+      getnumberofLikes();
+      Interactlikebutton();
+      itemsCounter(data.meals, recipeCount);
+    });
+};
+
+export default displayfoodCards;
